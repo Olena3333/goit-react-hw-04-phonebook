da0764f9d4bf7b807dbb3f702ffb5482a8199eed
Olena <helensmart2323@gmail.com>
2023-11-03 09:59:24 +0100

    Hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
@@ -6,96 +6,64 @@ import { Notificatio } from './Notificatio/Notificatio';
 import { StyledWrapper } from './App.styled';
 import { toast } from 'react-toastify';
 
-export class App extends React.Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     toast.success('Component was Mount');
     const contacts = JSON.parse(window.localStorage.getItem('contacts'));
     if (contacts?.length) {
-      this.setState({ contacts });
+      setContacts(contacts);
     }
-  }
+  }, []);
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      window.localStorage.setItem(
-        'contacts',
-        JSON.stringify(this.state.contacts)
-      );
-    }
-    if (prevState.filter !== this.state.filter) {
-      window.localStorage.setItem('filter', JSON.stringify(this.state.filter));
-    }
-  }
-
-  getFilterContacts = () => {
-    return this.state.contacts.filter(contact =>
-      contact.name
-        .trim()
-        .toLowerCase()
-        .includes(this.state.filter.trim().toLowerCase())
+  const getFilterContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.trim().toLowerCase().includes(filter.trim().toLowerCase())
     );
   };
 
-  handelOnFilter = e => {
-    const { target } = e;
-    const { name, value } = target;
-    this.setState({ [name]: value });
+  const handelOnFilter = e => {
+    setFilter(e.target.value);
   };
 
-  handleAddContact = newContact => {
-    const same = this.state.contacts.find(
-      contact => contact.name === newContact.name
-    );
+  const handleAddContact = newContact => {
+    const same = contacts.find(contact => contact.name === newContact.name);
 
     same
       ? toast.success(`${newContact.name} is already in contacts.`)
-      : this.setState({
-          contacts: [newContact, ...this.state.contacts],
-        });
+      : setContacts([newContact, ...contacts]);
   };
 
-  handleDeleteContact = id => {
-    this.setState(prev => ({
-      contacts: prev.contacts.filter(contact => contact.id !== id),
-    }));
-  };
-
-  render() {
-    const contacts = this.getFilterContacts();
-    return (
-      <StyledWrapper>
-        <h1>Phonebook</h1>
-
-        <ContactForm
-          onAddContact={this.handleAddContact}
-          // contacts={this.state.contacts}
-        />
-
-        <h2>Contacts</h2>
-
-        <Filter
-          filter={this.state.filter}
-          onFilterChange={this.handelOnFilter}
-        />
-        {this.state.contacts.length ? (
-          <ContactList
-            contacts={contacts}
-            deletedContact={this.handleDeleteContact}
-          />
-        ) : (
-          <Notificatio message={'You have no contacts'} />
-        )}
-      </StyledWrapper>
+  const handleDeleteContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
     );
-  }
-}
+  };
+
+  const filterContact = getFilterContacts();
+  return (
+    <StyledWrapper>
+      <h1>Phonebook</h1>
+
+      <ContactForm
+        onAddContact={handleAddContact}
+        // contacts={this.state.contacts}
+      />
+      <h2>Contacts</h2>
+      <Filter filter={filter} onFilterChange={handelOnFilter} />
+      {contacts.length ? (
+        <ContactList
+          contacts={filterContact}
+          deletedContact={handleDeleteContact}
+        />
+      ) : (
+        <Notificatio message={'You have no contacts'} />
+      )}
+    </StyledWrapper>
+  );
+};
